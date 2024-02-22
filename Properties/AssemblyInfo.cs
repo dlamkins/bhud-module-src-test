448a637faa4382080e9c6d59701dbfdc54755a94
BhudPkgReviewer <org@blishhud.com>
2024-02-22 02:01:00 +0000

    Marker Pack Assistant (HexedHero.Blish_HUD.MarkerPackAssistant) v1.0.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("MarkerPackAssistant")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0+d764c76813a70368bb35af32cbb0371c9fd49552")]
+[assembly: AssemblyInformationalVersion("1.0.0+29b1f416dd87eef5b920b479c2e6a8cebfe61859")]
 [assembly: AssemblyProduct("MarkerPackAssistant")]
 [assembly: AssemblyTitle("MarkerPackAssistant")]
 [assembly: AssemblyVersion("1.0.0.0")]
