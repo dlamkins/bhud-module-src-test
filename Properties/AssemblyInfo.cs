5156d3698f16d3c445215e117d3185a2edebe710
BhudPkgReviewer <org@blishhud.com>
2024-02-24 20:50:03 +0000

    Custom Combat Text (Ideka.CustomCombatText) v0.1.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("CustomCombatText")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0+725099bc0b6e67f2080f1e80af8968012625eabd")]
+[assembly: AssemblyInformationalVersion("1.0.0+6e4c1d06a1f98cb250b7f76da318a12f7a6f1281")]
 [assembly: AssemblyProduct("CustomCombatText")]
 [assembly: AssemblyTitle("CustomCombatText")]
 [assembly: AssemblyVersion("1.0.0.0")]
