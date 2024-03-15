2dd1e109821e3b4a50ccb3aea84bd177af0dcdac
BhudPkgReviewer <org@blishhud.com>
2024-03-15 20:24:58 +0000

    Live Map (estreya.bh.liveMap) v1.2.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("Estreya.BlishHUD.LiveMap")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0+dbc6185571ff214d7d1a86d44595948e8836d9ed")]
+[assembly: AssemblyInformationalVersion("1.0.0+0fca5606988a33028ccb6440565e7f15bc959644")]
 [assembly: AssemblyProduct("Estreya.BlishHUD.LiveMap")]
 [assembly: AssemblyTitle("Estreya.BlishHUD.LiveMap")]
 [assembly: NeutralResourcesLanguage("en")]
