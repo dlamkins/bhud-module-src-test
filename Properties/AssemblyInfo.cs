f9f5b180e4e074f2390dcac1f31192de52d55f3f
BhudPkgReviewer <org@blishhud.com>
2024-10-19 14:41:39 +0000

    Mounts & More (Manlaan.Mounts) v1.5.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("Mounts")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0+8283651144ee4debf58bf1591a8e588f1bfab3fd")]
+[assembly: AssemblyInformationalVersion("1.0.0+b3342e8e33b9d2c021297796ef2d7cf2a992cfa4")]
 [assembly: AssemblyProduct("Mounts")]
 [assembly: AssemblyTitle("Mounts")]
 [assembly: AssemblyVersion("1.0.0.0")]
