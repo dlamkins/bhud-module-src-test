2d4b2ff43fa3c6595dd904eed1492a7670b85857
BhudPkgReviewer <org@blishhud.com>
2024-12-04 02:08:42 +0000

    Clears Tracker (Soeed.RaidClears) v3.1.1

--- RaidClears/Settings/Views/ModuleMainSettingsView.cs
+++ RaidClears/Settings/Views/ModuleMainSettingsView.cs
@@ -2,7 +2,6 @@ using System;
 using Blish_HUD.Controls;
 using Blish_HUD.Graphics.UI;
 using Blish_HUD.Input;
-using Microsoft.Xna.Framework;
 using RaidClears.Localization;
 using RaidClears.Utils;
 
@@ -26,21 +25,6 @@ namespace RaidClears.Settings.Views
 			//IL_0049: Unknown result type (might be due to invalid IL or missing references)
 			//IL_004e: Unknown result type (might be due to invalid IL or missing references)
 			//IL_0059: Expected O, but got Unknown
-			//IL_0061: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0066: Unknown result type (might be due to invalid IL or missing references)
-			//IL_006d: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0078: Unknown result type (might be due to invalid IL or missing references)
-			//IL_007a: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0084: Unknown result type (might be due to invalid IL or missing references)
-			//IL_008e: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0090: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0095: Unknown result type (might be due to invalid IL or missing references)
-			//IL_009b: Unknown result type (might be due to invalid IL or missing references)
-			//IL_00a5: Unknown result type (might be due to invalid IL or missing references)
-			//IL_00aa: Unknown result type (might be due to invalid IL or missing references)
-			//IL_00af: Unknown result type (might be due to invalid IL or missing references)
-			//IL_00be: Unknown result type (might be due to invalid IL or missing references)
-			//IL_00c3: Unknown result type (might be due to invalid IL or missing references)
 			StandardButton val = new StandardButton();
 			((Control)val).set_Parent(buildPanel);
 			val.set_Text(Strings.ModuleSettings_OpenSettings);
@@ -48,16 +32,7 @@ namespace RaidClears.Settings.Views
 			((Control)val).set_Location(((Control)buildPanel).get_Size().Half() - ((Control)buildPanel).get_Size().Scale(0.2f).Half());
 			StandardButton _openSettingsButton = val;
 			buildPanel.AddControl((Control)(object)_openSettingsButton);
-			StandardButton val2 = new StandardButton();
-			((Control)val2).set_Parent(buildPanel);
-			val2.set_Text("Setup Wizard");
-			((Control)val2).set_Size(((Control)buildPanel).get_Size().Scale(0.2f));
-			((Control)val2).set_Location(((Control)buildPanel).get_Size().Half() - ((Control)buildPanel).get_Size().Scale(0.2f).Half() + new Point(0, ((Control)_openSettingsButton).get_Height() + 10));
 			((Control)_openSettingsButton).add_Click((EventHandler<MouseEventArgs>)delegate
-			{
-				((Control)Service.SettingsWindow).Show();
-			});
-			((Control)val2).add_Click((EventHandler<MouseEventArgs>)delegate
 			{
 				((Control)Service.SettingsWindow).Show();
 			});
