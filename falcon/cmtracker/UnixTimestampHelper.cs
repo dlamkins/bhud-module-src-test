2ee025db2e885033f04cf45fbc861c808a2bdc64
BhudPkgReviewer <org@blishhud.com>
2025-10-21 06:25:04 +0000

    CM Tracker (falcon.cmtracker) v1.2.2

--- falcon/cmtracker/UnixTimestampHelper.cs
+++ falcon/cmtracker/UnixTimestampHelper.cs
@@ -6,15 +6,15 @@ namespace falcon.cmtracker
 	{
 		public static long getLatestWeeklyServerResetTimestampInSeconds()
 		{
-			DateTimeOffset nowUtc = DateTimeOffset.UtcNow;
-			int daysSinceMonday = (int)(nowUtc.DayOfWeek - 1);
+			TimeSpan utcMinus6Offset = TimeSpan.FromHours(-6.0);
+			DateTimeOffset nowInUtcMinus6 = DateTime.UtcNow.Add(utcMinus6Offset);
+			int daysSinceMonday = (int)(nowInUtcMinus6.DayOfWeek - 1);
 			if (daysSinceMonday < 0)
 			{
 				daysSinceMonday = 7;
 			}
-			DateTimeOffset latestMondayUtc = nowUtc.AddDays(-daysSinceMonday);
-			TimeSpan utcMinus6Offset = TimeSpan.FromHours(-6.0);
-			return new DateTimeOffset(latestMondayUtc.Year, latestMondayUtc.Month, latestMondayUtc.Day, 1, 30, 0, utcMinus6Offset).ToUnixTimeSeconds();
+			DateTimeOffset latestMonday = nowInUtcMinus6.AddDays(-daysSinceMonday);
+			return new DateTimeOffset(latestMonday.Year, latestMonday.Month, latestMonday.Day, 1, 30, 0, utcMinus6Offset).ToUnixTimeSeconds();
 		}
 	}
 }
