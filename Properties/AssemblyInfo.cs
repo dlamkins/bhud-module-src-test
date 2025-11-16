57dbfe4ae06a1c8599d31626f2eaf328c1288949
BhudPkgReviewer <org@blishhud.com>
2025-11-16 14:55:39 +0000

    KpRefresher (japyx.kprefresher) v1.7.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("KpRefresher")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0+f8263f453b46ef6b93f50d9b8f0daecb2770d814")]
+[assembly: AssemblyInformationalVersion("1.0.0+03aeaebbd09b62e3b576a745a6cce85f94c4db0a")]
 [assembly: AssemblyProduct("KpRefresher")]
 [assembly: AssemblyTitle("KpRefresher")]
 [assembly: AssemblyVersion("1.0.0.0")]
