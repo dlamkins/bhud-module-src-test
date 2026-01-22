e887107922e0fd16d3ee1dbed0b152c60b07d98a
BhudPkgReviewer <org@blishhud.com>
2026-01-22 08:13:07 +0000

    WebPeeper (Yeh.WebPeeper) v1.0.3

---
+++ BhModule/WebPeeper/Strings/UIService.cs
@@ -0,0 +1,55 @@
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace BhModule.WebPeeper.Strings
+{
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal class UIService
+	{
+		private static ResourceManager resourceMan;
+
+		private static CultureInfo resourceCulture;
+
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				if (resourceMan == null)
+				{
+					resourceMan = new ResourceManager("BhModule.WebPeeper.Strings.UIService", typeof(UIService).Assembly);
+				}
+				return resourceMan;
+			}
+		}
+
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return resourceCulture;
+			}
+			set
+			{
+				resourceCulture = value;
+			}
+		}
+
+		internal static string Accept_Warning => ResourceManager.GetString("Accept_Warning", resourceCulture);
+
+		internal static string Hide_Warning => ResourceManager.GetString("Hide_Warning", resourceCulture);
+
+		internal static string Warning => ResourceManager.GetString("Warning", resourceCulture);
+
+		internal UIService()
+		{
+		}
+	}
+}
--- BhModule/WebPeeper/Window/Warning.cs
+++ BhModule/WebPeeper/Window/Warning.cs
@@ -1,7 +1,7 @@
 using System;
+using BhModule.WebPeeper.Strings;
 using Blish_HUD.Controls;
 using Blish_HUD.Input;
-using Microsoft.Xna.Framework;
 
 namespace BhModule.WebPeeper.Window
 {
@@ -20,38 +20,36 @@ namespace BhModule.WebPeeper.Window
 		public Warning(Action createWebPainter)
 			: this()
 		{
-			//IL_0026: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0031: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0036: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0041: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0049: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0051: Unknown result type (might be due to invalid IL or missing references)
-			//IL_005d: Expected O, but got Unknown
-			//IL_0075: Unknown result type (might be due to invalid IL or missing references)
-			//IL_007a: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0085: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0091: Expected O, but got Unknown
+			//IL_002e: Unknown result type (might be due to invalid IL or missing references)
+			//IL_0033: Unknown result type (might be due to invalid IL or missing references)
+			//IL_003e: Unknown result type (might be due to invalid IL or missing references)
+			//IL_004a: Expected O, but got Unknown
+			//IL_004b: Unknown result type (might be due to invalid IL or missing references)
+			//IL_0050: Unknown result type (might be due to invalid IL or missing references)
+			//IL_005b: Unknown result type (might be due to invalid IL or missing references)
+			//IL_0063: Unknown result type (might be due to invalid IL or missing references)
+			//IL_006b: Unknown result type (might be due to invalid IL or missing references)
+			//IL_0077: Expected O, but got Unknown
 			_createWebPainter = createWebPainter;
 			((FlowPanel)this).set_FlowDirection((ControlFlowDirection)3);
 			((Panel)this).set_CanScroll(true);
-			((FlowPanel)this).set_OuterControlPadding(new Vector2(0f, 10f));
-			StandardButton val = new StandardButton();
-			val.set_Text("Got It");
-			((Control)val).set_Width(100);
-			((Control)val).set_Height(30);
+			WarningContent warningContent = new WarningContent();
+			((Control)warningContent).set_Parent((Container)(object)this);
+			_content = warningContent;
+			Checkbox val = new Checkbox();
+			val.set_Text(BhModule.WebPeeper.Strings.UIService.Hide_Warning);
 			((Control)val).set_Parent((Container)(object)this);
-			_acceptBtn = val;
+			_alwaysHideCheckbox = val;
+			StandardButton val2 = new StandardButton();
+			val2.set_Text(BhModule.WebPeeper.Strings.UIService.Accept_Warning);
+			((Control)val2).set_Width(100);
+			((Control)val2).set_Height(30);
+			((Control)val2).set_Parent((Container)(object)this);
+			_acceptBtn = val2;
 			((Control)_acceptBtn).add_Click((EventHandler<MouseEventArgs>)delegate
 			{
 				Accept();
 			});
-			Checkbox val2 = new Checkbox();
-			val2.set_Text("Don't show again.");
-			((Control)val2).set_Parent((Container)(object)this);
-			_alwaysHideCheckbox = val2;
-			WarningContent warningContent = new WarningContent();
-			((Control)warningContent).set_Parent((Container)(object)this);
-			_content = warningContent;
 		}
 
 		protected override void OnResized(ResizedEventArgs e)
@@ -63,16 +61,17 @@ namespace BhModule.WebPeeper.Window
 
 		public override void RecalculateLayout()
 		{
-			//IL_0026: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0049: Unknown result type (might be due to invalid IL or missing references)
-			//IL_0063: Unknown result type (might be due to invalid IL or missing references)
-			//IL_007b: Unknown result type (might be due to invalid IL or missing references)
-			//IL_00a1: Unknown result type (might be due to invalid IL or missing references)
+			//IL_0033: Unknown result type (might be due to invalid IL or missing references)
+			//IL_0056: Unknown result type (might be due to invalid IL or missing references)
+			//IL_0070: Unknown result type (might be due to invalid IL or missing references)
+			//IL_0089: Unknown result type (might be due to invalid IL or missing references)
+			//IL_00af: Unknown result type (might be due to invalid IL or missing references)
 			((FlowPanel)this).RecalculateLayout();
 			if (_content != null && _acceptBtn != null && _alwaysHideCheckbox != null)
 			{
+				((Control)_content).set_Left(15);
 				((Control)_content).set_Height(((Container)this).get_ContentRegion().Height - ((Control)_acceptBtn).get_Height() - ((Control)_alwaysHideCheckbox).get_Height() - (int)((FlowPanel)this).get_OuterControlPadding().Y - 1);
-				((Control)_content).set_Width(((Container)this).get_ContentRegion().Width - 1);
+				((Control)_content).set_Width(((Container)this).get_ContentRegion().Width - 30);
 				((Control)_acceptBtn).set_Left(((Container)this).get_ContentRegion().Width / 2 - ((Control)_acceptBtn).get_Width() / 2);
 				((Control)_alwaysHideCheckbox).set_Left(((Container)this).get_ContentRegion().Width / 2 - ((Control)_alwaysHideCheckbox).get_Width() / 2);
 			}
