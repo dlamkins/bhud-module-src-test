29a386d4248442ceecc06ed4cbbe7b0c1a2ddacb
BhudPkgReviewer <org@blishhud.com>
2026-02-01 19:44:14 +0000

    V.E.L.O.C.I.T.Y. (Taimi.Velocity) v1.2.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("Velocity")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0+b58d5bf30b632feb873e1fffd619f4166ee359b7")]
+[assembly: AssemblyInformationalVersion("1.0.0+cc1be4dd31f2d2e6d03f22d21994075a298efbb5")]
 [assembly: AssemblyProduct("Velocity")]
 [assembly: AssemblyTitle("Velocity")]
 [assembly: AssemblyVersion("1.0.0.0")]
