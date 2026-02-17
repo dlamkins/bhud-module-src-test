3b5f8f7e9b9dcedf258cac0dcdbb0022c75255c8
BhudPkgReviewer <org@blishhud.com>
2026-02-17 22:12:35 +0000

    RaidWeekPlanner (japyx.raidweekplanner) v1.0.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("RaidWeekPlanner")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0+d51702d2b207ef1e85ca528b4697aceab628ff31")]
+[assembly: AssemblyInformationalVersion("1.0.0+91f5564dd3e484d32c3513db4f90fd69fa0f5ae3")]
 [assembly: AssemblyProduct("RaidWeekPlanner")]
 [assembly: AssemblyTitle("RaidWeekPlanner")]
 [assembly: AssemblyVersion("1.0.0.0")]
