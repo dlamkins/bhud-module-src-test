2e6903df7c72d27b47888b272aac368518b73b1d
BhudPkgReviewer <org@blishhud.com>
2026-03-08 23:54:37 +0000

    Gilled Wars (Gorthax.GilledWars) v3.2.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyCompany("GilledWars")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0+8b951e72146f167434b92890b7c84a8ab003ee36")]
+[assembly: AssemblyInformationalVersion("1.0.0+82c3e3984c34b29ab3823b5626d8a172e8cffd44")]
 [assembly: AssemblyProduct("GilledWars")]
 [assembly: AssemblyTitle("GilledWars")]
 [assembly: AssemblyVersion("1.0.0.0")]
